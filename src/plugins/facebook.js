0808be3b911a834e6554a082101b526b68d4eb26
xingwanyue <xwymail@qq.com>
2024-11-27 11:23:12 +0800

    facebook plugin

---
+++ src/plugins/facebook.js
@@ -0,0 +1,23 @@
+export default defineNuxtPlugin(() => {
+  const init = (f, b, e, v, n, t, s) => {
+    if (f.fbq) return;
+    n = f.fbq = function () {
+      n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+    };
+    if (!f._fbq) f._fbq = n;
+    n.push = n;
+    n.loaded = !0;
+    n.version = '2.0';
+    n.queue = [];
+    t = b.createElement(e);
+    t.async = !0;
+    t.src = v;
+    s = b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t, s);
+  };
+  setTimeout(function () {
+    init(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '3700958203515627');
+    fbq('track', 'PageView');
+  }, 1000);
+});
