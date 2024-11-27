741c348ad868d5076bd1351c9d15593144fc80b0
xingwanyue <xwymail@qq.com>
2024-11-27 12:23:32 +0800

    facebook plugin

--- src/plugins/facebook.js
+++ src/plugins/facebook.js
@@ -15,18 +15,23 @@ export default defineNuxtPlugin(() => {
     s = b.getElementsByTagName(e)[0];
     s.parentNode.insertBefore(t, s);
   };
+  const appendImg = () => {
+    const d = document;
+    const s = d.createElement('img');
+    const t = d.createElement('noscript');
+
+    s.src = 'https://www.facebook.com/tr?id=3700958203515627&ev=PageView&noscript=1';
+    s.height = '1';
+    s.width = '1';
+    s.style.display = 'none';
+    t.appendChild(s);
+    document.body.appendChild(t);
+  };
   setTimeout(function () {
     init(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
     fbq('init', '3700958203515627');
     fbq('track', 'PageView');
-  }, 1000);
 
-  <noscript>
-    <img
-      height='1'
-      width='1'
-      style='display: none'
-      src='https://www.facebook.com/tr?id=3700958203515627&ev=PageView&noscript=1'
-    />
-  </noscript>;
+    appendImg();
+  }, 1000);
 });
