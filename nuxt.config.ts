c961698cdb6b4243845d7f8f5efbdde50f57e2a7
xingwanyue <xwymail@qq.com>
2024-12-17 10:13:37 +0800

    像素代码

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -118,14 +118,14 @@ export default defineNuxtConfig({
       s.parentNode.insertBefore(t, s)
     }(window, document, 'script',
       'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '1316751486182168');
+    fbq('init', '1314035249495524');
     fbq('track', 'PageView');`,
         },
       ],
       noscript: [
         {
           innerHTML: `<img height="1" width="1" style="display:none"
-      src="https://www.facebook.com/tr?id=1316751486182168&ev=PageView&noscript=1">`
+      src="https://www.facebook.com/tr?id=1314035249495524&ev=PageView&noscript=1">`
         }
       ],
       title: 'DET Practice - Ace the Duolingo English Test',
