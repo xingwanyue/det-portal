2f6873d5376eb0f0f00b159d3931dad040966c4b
xingwanyue <xingwanyue@gmail.com>
2025-02-25 18:28:32 +0800

    api

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -131,7 +131,7 @@ export default defineNuxtConfig({
       title: 'DET Practice - Ace the Duolingo English Test',
       meta: [
         { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no' },
         { name: 'msvalidate.01', content: 'AB8D0671C60241C25BF7B5B9E68F5683' },
       ],
     },
