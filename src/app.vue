159eca5deb19f4ed382a11394a153d7e9cfa4277
xingwanyue <xingwanyue@gmail.com>
2025-02-27 19:08:30 +0800

    log path

--- src/app.vue
+++ src/app.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+onMounted(() => {
+  fetch(`https://www.duolingopractice.com/weapp/api/common/logPath/${encodeURIComponent('/prep')}`);
+});
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
