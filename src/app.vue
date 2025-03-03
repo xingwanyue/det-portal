2147ecf4267413485fec6b0509d395c333dd60b9
xingwanyue <xingwanyue@gmail.com>
2025-03-03 09:20:57 +0800

    logpath

--- src/app.vue
+++ src/app.vue
@@ -2,6 +2,10 @@
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
+import { useI18n } from 'vue-i18n';
+const { locale } = useI18n();
+const localePath = useLocalePath();
+const currentPathWithoutLocale = route.path.replace(new RegExp(`^/${locale.value}`), '') || '/';
 
 watch(
   () => route.path,
@@ -10,7 +14,7 @@ watch(
       fetch(
         `https://www.duolingopractice.com/weapp/api/common/logPath?path=${encodeURIComponent(
           window.location.pathname,
-        )}`,
+        )}&locale=${locale.value}`,
       );
     }
   },
