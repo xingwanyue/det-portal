8f0d639be5ae2fd455c5dc904e43e14d72acf39c
xingwanyue <xingwanyue@gmail.com>
2025-03-04 09:39:18 +0800

    test

--- src/app.vue
+++ src/app.vue
@@ -1,30 +1,3 @@
-<!-- <script setup lang="ts">
-import { useRoute } from 'vue-router';
-
-const route = useRoute();
-import { useI18n } from 'vue-i18n';
-const { locale } = useI18n();
-const currentPathWithoutLocale = route.path.replace(new RegExp(`^/${locale.value}`), '') || '/';
-
-watch(
-  () => route.path,
-  (val) => {
-    if (val && process.client) {
-      try {
-        useFetch(
-          `https://www.duolingopractice.com/weapp/api/common/logPath?path=${encodeURIComponent(
-            currentPathWithoutLocale,
-          )}&locale=${locale.value}`,
-        );
-      } catch (e) {
-        //
-      }
-    }
-  },
-  { immediate: true },
-);
-</script> -->
-
 <template>
   <NuxtLayout>
     <NuxtPage />
