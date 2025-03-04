78ba629e7dd32d9be0cacab6cac9384c28994a91
xingwanyue <xingwanyue@gmail.com>
2025-03-04 09:23:03 +0800

    fetch

--- src/app.vue
+++ src/app.vue
@@ -4,18 +4,21 @@ import { useRoute } from 'vue-router';
 const route = useRoute();
 import { useI18n } from 'vue-i18n';
 const { locale } = useI18n();
-const localePath = useLocalePath();
 const currentPathWithoutLocale = route.path.replace(new RegExp(`^/${locale.value}`), '') || '/';
 
 watch(
   () => route.path,
   (val) => {
     if (val && process.client) {
-      fetch(
-        `https://www.duolingopractice.com/weapp/api/common/logPath?path=${encodeURIComponent(
-          window.location.pathname,
-        )}&locale=${locale.value}`,
-      );
+      try {
+        useFetch(
+          `https://www.duolingopractice.com/weapp/api/common/logPath?path=${encodeURIComponent(
+            currentPathWithoutLocale,
+          )}&locale=${locale.value}`,
+        );
+      } catch (e) {
+        //
+      }
     }
   },
   { immediate: true },
