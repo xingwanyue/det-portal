ce85319065aba26f1c316cdc553d1a31c9128538
xingcongcong <xingcongcong@henanyuexue.com>
2025-03-06 14:44:09 +0800

    new page

---
+++ src/pages/det-course-speaking-sample.vue
@@ -0,0 +1,353 @@
+<script lang="ts" setup>
+import { useI18n } from 'vue-i18n';
+const { t } = useI18n();
+import my_video from '@/components/prepCompoment/video.vue';
+const showAnswer = ref(false);
+const video_data = computed(() => {
+  return {
+    video_title: 'det-prep-course-read-aloud',
+    video_url: 'https://www.youtube.com/embed/sf8FN3GAuHI?feature=oembed',
+  };
+});
+</script>
+<template>
+  <div class="common_prep_wrapper det-prep-course-read-aloud">
+    <div class="common_prep_out">
+      <div class="common_prep_left">
+        <img /> ****
+        <h1></h1>
+        <p></p>
+        <p></p>
+        <h2></h2>
+        <p></p>
+        <ul></ul>
+        <p></p>
+        <ul></ul>
+        <p></p>
+        <p></p>
+        <ul></ul>
+        <p></p>
+        <ul></ul>
+        <p></p>
+        <ul></ul>
+        <p></p>
+        <ul></ul>
+        <p></p>
+        <ul></ul>
+        <p></p>
+        <ul></ul>
+        <p></p>
+        <ul></ul>
+        <h2></h2>
+        <p></p>
+        <p></p>
+        <h2></h2>
+        <p></p>
+        <p></p>
+        <img /> ****
+        <figcaption></figcaption>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <img /> ****
+        <figcaption></figcaption>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <h2></h2>
+        <p></p>
+        <img /> ****
+        <figcaption></figcaption>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <h2></h2>
+        <p></p>
+        <p></p>
+        <p></p>
+        <img /> ****
+        <figcaption></figcaption>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <img /> ****
+        <figcaption></figcaption>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <h2></h2>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <h2></h2>
+        <p></p>
+        <img /> ****
+        <figcaption></figcaption>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <h2></h2>
+        <p></p>
+        <img /> ****
+        <figcaption></figcaption>
+        <p></p>
+        <h3></h3>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <h2></h2>
+        <p></p>
+        <ul></ul>
+        <p></p>
+        <p></p>
+        <p></p>
+        <ul></ul>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <ul></ul>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <ul></ul>
+        <p></p>
+        <p></p>
+        <ul></ul>
+        <p></p>
+        <p></p>
+        <ul></ul>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <ul></ul>
+        <p></p>
+        <p></p>
+        <img /> ****
+      </div>
+      <div class="common_prep_right"></div>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+@import '@/assets/prep.scss';
+</style>
+<style lang="scss">
+.det-prep-course-read-aloud {
+  .sub-item {
+    margin-left: 20px;
+  }
+  .title {
+    text-align: center;
+    font-weight: bold;
+    font-size: 1.5em;
+  }
+  ul {
+    list-style-type: none;
+    padding-left: 0;
+    li {
+      margin-bottom: 15px;
+    }
+    .header {
+      background-color: yellow;
+      font-weight: bold;
+
+      padding: 2px 5px;
+    }
+    .blue {
+      color: blue;
+    }
+    .indent {
+      margin-left: 20px;
+    }
+    .red {
+      color: red;
+    }
+    .sub-indent {
+      margin-left: 40px;
+    }
+    .purple {
+      color: purple;
+      text-decoration: underline;
+    }
+  }
+  p {
+    .blue {
+      color: blue;
+    }
+    .red {
+      color: red;
+    }
+  }
+  .html_wrapper {
+    .red-text {
+      color: #ff3300;
+    }
+    .blue-text {
+      color: #3366cc;
+    }
+    .orange-text {
+      color: orange;
+    }
+    .has-accent-3-color {
+      color: orange !important;
+    }
+  }
+  .wp-block-list {
+    margin-left: 0px;
+    padding-left: 20px;
+  }
+  .answer_out {
+    margin-top: 20px;
+    padding: 15px;
+    background-color: #f1f1f1;
+    border-radius: 4px;
+    text-align: left;
+    font-size: 16px;
+    line-height: 26px;
+
+    color: rgb(17, 17, 17);
+    background-color: rgb(241, 241, 241);
+  }
+}
+</style>
