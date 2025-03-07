81f241ceb6e86c63fd0f3fa658bf0a52960b0a9c
xingcongcong <xingcongcong@henanyuexue.com>
2025-03-07 10:00:11 +0800

    boost_det_vocabulary_7_adv_photo_desc_tech

---
+++ src/pages/boost-det-vocabulary-7-adv-photo-desc-tech.vue
@@ -0,0 +1,342 @@
+<script lang="ts" setup>
+import { useI18n } from 'vue-i18n';
+const { t } = useI18n();
+import my_video from '@/components/prepCompoment/video.vue';
+
+const video_data = computed(() => {
+  return {
+    video_title: 'boost-det-vocabulary-7-adv-photo-desc-tech',
+    video_url: 'https://www.youtube.com/embed/4FMRfkIKsvQ?si=eaVIa3b7vbRNGvam',
+  };
+});
+</script>
+<template>
+  <div class="common_prep_wrapper boost-det-vocabulary-7-adv-photo-desc-tech">
+    <div class="common_prep_out">
+      <div class="common_prep_left">
+        <div class="banner_img">
+          <img
+            src="/img/boost_det_vocabulary_7_adv_photo_desc_tech/Red-White-Modern-Youtube-Thumbnail-1.jpg"
+            alt="boost-det-vocabulary-7-adv-photo-desc-tech"
+          />
+        </div>
+
+        <h1>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.h11') }}</h1>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p1')"></div>
+        <h2>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.h21') }}</h2>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.ol1')"></div>
+        <h2>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.h22') }}</h2>
+
+        <p>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.p2') }}</p>
+
+        <iframe
+          data-src="https://podcasters.spotify.com/pod/show/duolingoenglishtest/embed/episodes/Boost-Your-DET-Vocabulary-7-Advanced-Photo-Description-Techniques-e2qtpst/a-abkjvuv"
+          height="102px"
+          frameborder="0"
+          scrolling="no"
+        ></iframe>
+
+        <h2>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.h23') }}</h2>
+        <p>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.p3') }}</p>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.ol2')"></div>
+        <p>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.p4') }}</p>
+        <h2>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.h24') }}</h2>
+        <p>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.p5') }}</p>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p6')"></div>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.ul1')"></div>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p7')"></div>
+        <img
+          src="/img/boost_det_vocabulary_7_adv_photo_desc_tech/001.png"
+          :alt="t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption1')"
+        />
+        <figcaption>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption1') }}</figcaption>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.ul2')"></div>
+        <img
+          src="/img/boost_det_vocabulary_7_adv_photo_desc_tech/002.png"
+          :alt="t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption2')"
+        />
+        <figcaption>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption2') }}</figcaption>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.ul3')"></div>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p8')"></div>
+        <img
+          src="/img/boost_det_vocabulary_7_adv_photo_desc_tech/003.jpg"
+          :alt="t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption3')"
+        />
+        <figcaption>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption3') }}</figcaption>
+        <p>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.p9') }}</p>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p10')"></div>
+        <p>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.p11') }}</p>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p12')"></div>
+        <h2>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.h25') }}</h2>
+        <p>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.p13') }}</p>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p14')"></div>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.ul4')"></div>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p15')"></div>
+        <img
+          src="/img/boost_det_vocabulary_7_adv_photo_desc_tech/004.jpg"
+          :alt="t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption4')"
+        />
+        <figcaption>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption4') }}</figcaption>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.ul5')"></div>
+        <img
+          src="/img/boost_det_vocabulary_7_adv_photo_desc_tech/005.png"
+          :alt="t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption5')"
+        />
+        <figcaption>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption5') }}</figcaption>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.ul6')"></div>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p16')"></div>
+        <img
+          src="/img/boost_det_vocabulary_7_adv_photo_desc_tech/006.jpg"
+          :alt="t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption6')"
+        />
+        <figcaption>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption6') }}</figcaption>
+        <p>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.p17') }}</p>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p18')"></div>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p19')"></div>
+        <h2>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.h26') }}</h2>
+        <p>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.p20') }}</p>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p21')"></div>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.ul7')"></div>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p22')"></div>
+        <img
+          src="/img/boost_det_vocabulary_7_adv_photo_desc_tech/007.jpg"
+          :alt="t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption7')"
+        />
+        <figcaption>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption7') }}</figcaption>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.ul8')"></div>
+        <img
+          src="/img/boost_det_vocabulary_7_adv_photo_desc_tech/008.jpg"
+          :alt="t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption8')"
+        />
+        <figcaption>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption8') }}</figcaption>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.ul9')"></div>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p23')"></div>
+        <img
+          src="/img/boost_det_vocabulary_7_adv_photo_desc_tech/009.jpg"
+          :alt="t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption9')"
+        />
+        <figcaption>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption9') }}</figcaption>
+        <p>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.p24') }}</p>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p25')"></div>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p26')"></div>
+        <h2>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.h27') }}</h2>
+        <p>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.p27') }}</p>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p28')"></div>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.ul10')"></div>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p29')"></div>
+        <img
+          src="/img/boost_det_vocabulary_7_adv_photo_desc_tech/010.jpg"
+          :alt="t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption10')"
+        />
+        <figcaption>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption10') }}</figcaption>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.ul11')"></div>
+        <img
+          src="/img/boost_det_vocabulary_7_adv_photo_desc_tech/011.jpg"
+          :alt="t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption11')"
+        />
+        <figcaption>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption11') }}</figcaption>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.ul12')"></div>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p30')"></div>
+        <img
+          src="/img/boost_det_vocabulary_7_adv_photo_desc_tech/012.jpg"
+          :alt="t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption12')"
+        />
+        <figcaption>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption12') }}</figcaption>
+        <p>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.p31') }}</p>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p32')"></div>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p33')"></div>
+        <h2>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.h28') }}</h2>
+        <p>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.p34') }}</p>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p35')"></div>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.ul13')"></div>
+        <img
+          src="/img/boost_det_vocabulary_7_adv_photo_desc_tech/013.png"
+          :alt="t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption13')"
+        />
+        <figcaption>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption13') }}</figcaption>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.ul14')"></div>
+        <img
+          src="/img/boost_det_vocabulary_7_adv_photo_desc_tech/014.png"
+          :alt="t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption14')"
+        />
+        <figcaption>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption14') }}</figcaption>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.ul15')"></div>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p37')"></div>
+        <img
+          src="/img/boost_det_vocabulary_7_adv_photo_desc_tech/015.png"
+          :alt="t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption15')"
+        />
+        <figcaption>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption15') }}</figcaption>
+        <p>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.p38') }}</p>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p39')"></div>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p40')"></div>
+        <h2>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.h29') }}</h2>
+        <p>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.p41') }}</p>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p42')"></div>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.ul16')"></div>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p43')"></div>
+        <img
+          src="/img/boost_det_vocabulary_7_adv_photo_desc_tech/016.png"
+          :alt="t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption16')"
+        />
+        <figcaption>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption16') }}</figcaption>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.ul17')"></div>
+        <img
+          src="/img/boost_det_vocabulary_7_adv_photo_desc_tech/017.png"
+          :alt="t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption17')"
+        />
+        <figcaption>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption17') }}</figcaption>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.ul18')"></div>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p44')"></div>
+        <img
+          src="/img/boost_det_vocabulary_7_adv_photo_desc_tech/018.png"
+          :alt="t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption18')"
+        />
+        <figcaption>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption18') }}</figcaption>
+        <p>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.p45') }}</p>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p46')"></div>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p47')"></div>
+        <h2>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.h210') }}</h2>
+        <p>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.p48') }}</p>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p49')"></div>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.ul19')"></div>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p50')"></div>
+        <img
+          src="/img/boost_det_vocabulary_7_adv_photo_desc_tech/019.png"
+          :alt="t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption19')"
+        />
+        <figcaption>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption19') }}</figcaption>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.ul20')"></div>
+        <img
+          src="/img/boost_det_vocabulary_7_adv_photo_desc_tech/020.jpg"
+          :alt="t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption20')"
+        />
+        <figcaption>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption20') }}</figcaption>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.ul21')"></div>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p51')"></div>
+        <img
+          src="/img/boost_det_vocabulary_7_adv_photo_desc_tech/021.png"
+          :alt="t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption21')"
+        />
+        <figcaption>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.figcaption21') }}</figcaption>
+        <p>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.p52') }}</p>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p53')"></div>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p54')"></div>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p55')"></div>
+        <h2>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.h211') }}</h2>
+        <p>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.p56') }}</p>
+        <p>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.p57') }}</p>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.ol3')"></div>
+        <p>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.p58') }}</p>
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p59')"></div>
+        <p>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.p60') }}</p>
+        <p>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.p61') }}</p>
+        <img src="/img/boost_det_vocabulary_7_adv_photo_desc_tech/image-22.jpg" />
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p62')"></div>
+        <p>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.p63') }}</p>
+        <img src="/img/boost_det_vocabulary_7_adv_photo_desc_tech/image-23.jpg" />
+        <div class="html_wrapper" v-html="t('boost_det_vocabulary_7_adv_photo_desc_tech.p64')"></div>
+        <p>{{ t('boost_det_vocabulary_7_adv_photo_desc_tech.p65') }}</p>
+        <!-- <img /> ****img*** -->
+      </div>
+      <div class="common_prep_right"></div>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+@import '@/assets/prep.scss';
+</style>
+<style lang="scss">
+.boost-det-vocabulary-7-adv-photo-desc-tech {
+  .sub-item {
+    margin-left: 20px;
+  }
+  .orange {
+    color: orange;
+  }
+  .bg_yellow {
+    background-color: yellow;
+    width: fit-content;
+  }
+  .title {
+    text-align: center;
+    font-weight: bold;
+    font-size: 1.5em;
+  }
+  li {
+    .highlight {
+      background-color: yellow;
+    }
+  }
+  ul {
+    list-style-type: none;
+    padding-left: 16px;
+    li {
+    }
+    .header {
+      background-color: yellow;
+      font-weight: bold;
+
+      padding: 2px 5px;
+    }
+    .blue {
+      color: blue;
+    }
+    .indent {
+      margin-left: 20px;
+    }
+    .red {
+      color: red;
+    }
+    .sub-indent {
+      margin-left: 40px;
+    }
+    .purple {
+      color: purple;
+      text-decoration: underline;
+    }
+  }
+  p {
+    .blue {
+      color: blue;
+    }
+    .red {
+      color: red;
+    }
+  }
+  .html_wrapper {
+    .red-text {
+      color: #ff3300;
+    }
+    .blue-text {
+      color: #3366cc;
+    }
+    .orange-text {
+      color: orange;
+    }
+    .has-accent-3-color {
+      color: orange !important;
+    }
+  }
+  .wp-block-list {
+    margin-left: 0px;
+    padding-left: 20px;
+  }
+  .answer_out {
+    margin-top: 20px;
+    padding: 15px;
+    background-color: #f1f1f1;
+    border-radius: 4px;
+    text-align: left;
+    font-size: 16px;
+    line-height: 26px;
+
+    color: rgb(17, 17, 17);
+    background-color: rgb(241, 241, 241);
+  }
+  img {
+    max-width: 400px;
+    margin-top: 4px;
+  }
+}
+</style>
