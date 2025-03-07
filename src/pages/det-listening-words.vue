021aef4a078f02a65649ec42d48c8e1a93e718bd
xingcongcong <xingcongcong@henanyuexue.com>
2025-03-07 11:38:23 +0800

    文案

---
+++ src/pages/det-listening-words.vue
@@ -0,0 +1,274 @@
+<script lang="ts" setup>
+import { useI18n } from 'vue-i18n';
+const { t } = useI18n();
+</script>
+<template>
+  <div class="common_prep_wrapper det-listening-words">
+    <div class="common_prep_out">
+      <div class="common_prep_left">
+        <img /> ******img
+        <h1>{{ t('det_listening_words.h11') }}</h1>
+        <h2>{{ t('det_listening_words.h21') }}</h2>
+        <p>{{ t('det_listening_words.p1') }}</p>
+        <p>{{ t('det_listening_words.p2') }}</p>
+        <p>{{ t('det_listening_words.p3') }}</p>
+        <p>{{ t('det_listening_words.p4') }}</p>
+        <p>{{ t('det_listening_words.p5') }}</p>
+        <p>{{ t('det_listening_words.p6') }}</p>
+        ***
+        <p>{{ t('det_listening_words.p7') }}</p>
+        <!-- <p>{{ t('det_listening_words.p8') }}</p> -->
+        ***iframe
+        <iframe
+          class="lazyload"
+          data-src="https://creators.spotify.com/pod/show/duolingoenglishtest/embed/episodes/Unlocking-DET-Interactive-Listening-with-Scenario-Specific-Vocabulary-e2r14kq/a-abko2cv"
+          height="102px"
+          width="400px"
+          frameborder="0"
+          scrolling="no"
+        ></iframe>
+        ***iframe
+        <h2>{{ t('det_listening_words.h22') }}</h2>
+        <p>{{ t('det_listening_words.p9') }}</p>
+        <p>{{ t('det_listening_words.p10') }}</p>
+        <p>{{ t('det_listening_words.p11') }}</p>
+        <h3>{{ t('det_listening_words.h31') }}</h3>
+        <p>{{ t('det_listening_words.p12') }}</p>
+        <div class="html_wrapper" v-html="t('det_listening_words.p13')"></div>
+        <p>{{ t('det_listening_words.p14') }}</p>
+        <p>{{ t('det_listening_words.p15') }}</p>
+        <p>{{ t('det_listening_words.p16') }}</p>
+        <p>{{ t('det_listening_words.p17') }}</p>
+        <div class="html_wrapper" v-html="t('det_listening_words.p18')"></div>
+        <p>{{ t('det_listening_words.p19') }}</p>
+        <p>{{ t('det_listening_words.p20') }}</p>
+        <p>{{ t('det_listening_words.p21') }}</p>
+        <div class="html_wrapper" v-html="t('det_listening_words.p22')"></div>
+        <p>{{ t('det_listening_words.p23') }}</p>
+        <p>{{ t('det_listening_words.p24') }}</p>
+        <p>{{ t('det_listening_words.p25') }}</p>
+        <p>{{ t('det_listening_words.p26') }}</p>
+        <p>{{ t('det_listening_words.p27') }}</p>
+        <h2>{{ t('det_listening_words.h23') }}</h2>
+        <p>{{ t('det_listening_words.p28') }}</p>
+        <p>{{ t('det_listening_words.p29') }}</p>
+        <p>{{ t('det_listening_words.p30') }}</p>
+        <p>{{ t('det_listening_words.p31') }}</p>
+        <p>{{ t('det_listening_words.p32') }}</p>
+        <p>{{ t('det_listening_words.p33') }}</p>
+        <p>{{ t('det_listening_words.p34') }}</p>
+        <p>{{ t('det_listening_words.p35') }}</p>
+        <p>{{ t('det_listening_words.p36') }}</p>
+        <p>{{ t('det_listening_words.p37') }}</p>
+        <p>{{ t('det_listening_words.p38') }}</p>
+        <p>{{ t('det_listening_words.p39') }}</p>
+        <p>{{ t('det_listening_words.p40') }}</p>
+        <p>{{ t('det_listening_words.p41') }}</p>
+        <p>{{ t('det_listening_words.p42') }}</p>
+        <p>{{ t('det_listening_words.p43') }}</p>
+        <p>{{ t('det_listening_words.p44') }}</p>
+        <p>{{ t('det_listening_words.p45') }}</p>
+        <p>{{ t('det_listening_words.p46') }}</p>
+        <p>{{ t('det_listening_words.p47') }}</p>
+        <p>{{ t('det_listening_words.p48') }}</p>
+        <p>{{ t('det_listening_words.p49') }}</p>
+        <p>{{ t('det_listening_words.p50') }}</p>
+        <p>{{ t('det_listening_words.p51') }}</p>
+        <p>{{ t('det_listening_words.p52') }}</p>
+        <p>{{ t('det_listening_words.p53') }}</p>
+        <p>{{ t('det_listening_words.p54') }}</p>
+        <p>{{ t('det_listening_words.p55') }}</p>
+        <p>{{ t('det_listening_words.p56') }}</p>
+        <p>{{ t('det_listening_words.p57') }}</p>
+        <h2>{{ t('det_listening_words.h24') }}</h2>
+        <p>{{ t('det_listening_words.p58') }}</p>
+        <p>{{ t('det_listening_words.p59') }}</p>
+        <p>{{ t('det_listening_words.p60') }}</p>
+        <p>{{ t('det_listening_words.p61') }}</p>
+        <p>{{ t('det_listening_words.p62') }}</p>
+        <p>{{ t('det_listening_words.p63') }}</p>
+        <p>{{ t('det_listening_words.p64') }}</p>
+        <p>{{ t('det_listening_words.p65') }}</p>
+        <p>{{ t('det_listening_words.p66') }}</p>
+        <p>{{ t('det_listening_words.p67') }}</p>
+        <p>{{ t('det_listening_words.p68') }}</p>
+        <p>{{ t('det_listening_words.p69') }}</p>
+        <p>{{ t('det_listening_words.p70') }}</p>
+        <p>{{ t('det_listening_words.p71') }}</p>
+        <p>{{ t('det_listening_words.p72') }}</p>
+        <p>{{ t('det_listening_words.p73') }}</p>
+        <p>{{ t('det_listening_words.p74') }}</p>
+        <p>{{ t('det_listening_words.p75') }}</p>
+        <p>{{ t('det_listening_words.p76') }}</p>
+        <p>{{ t('det_listening_words.p77') }}</p>
+        <p>{{ t('det_listening_words.p78') }}</p>
+        <p>{{ t('det_listening_words.p79') }}</p>
+        <p>{{ t('det_listening_words.p80') }}</p>
+        <p>{{ t('det_listening_words.p81') }}</p>
+        <p>{{ t('det_listening_words.p82') }}</p>
+        <p>{{ t('det_listening_words.p83') }}</p>
+        <p>{{ t('det_listening_words.p84') }}</p>
+        <p>{{ t('det_listening_words.p85') }}</p>
+        <p>{{ t('det_listening_words.p86') }}</p>
+        <p>{{ t('det_listening_words.p87') }}</p>
+        <p>{{ t('det_listening_words.p88') }}</p>
+        <p>{{ t('det_listening_words.p89') }}</p>
+        <p>{{ t('det_listening_words.p90') }}</p>
+        <p>{{ t('det_listening_words.p91') }}</p>
+        <p>{{ t('det_listening_words.p92') }}</p>
+        <p>{{ t('det_listening_words.p93') }}</p>
+        <p>{{ t('det_listening_words.p94') }}</p>
+        <p>{{ t('det_listening_words.p95') }}</p>
+        <p>{{ t('det_listening_words.p96') }}</p>
+        <p>{{ t('det_listening_words.p97') }}</p>
+        <p>{{ t('det_listening_words.p98') }}</p>
+        <p>{{ t('det_listening_words.p99') }}</p>
+        <p>{{ t('det_listening_words.p100') }}</p>
+        <p>{{ t('det_listening_words.p101') }}</p>
+        <p>{{ t('det_listening_words.p102') }}</p>
+        <p>{{ t('det_listening_words.p103') }}</p>
+        <p>{{ t('det_listening_words.p104') }}</p>
+        <h2>{{ t('det_listening_words.h25') }}</h2>
+        <p>{{ t('det_listening_words.p105') }}</p>
+        <p>{{ t('det_listening_words.p106') }}</p>
+        <p>{{ t('det_listening_words.p107') }}</p>
+        <p>{{ t('det_listening_words.p108') }}</p>
+        <p>{{ t('det_listening_words.p109') }}</p>
+        <p>{{ t('det_listening_words.p110') }}</p>
+        <p>{{ t('det_listening_words.p111') }}</p>
+        <p>{{ t('det_listening_words.p112') }}</p>
+        <p>{{ t('det_listening_words.p113') }}</p>
+        <div class="html_wrapper" v-html="t('det_listening_words.p114')"></div>
+        <div class="html_wrapper" v-html="t('det_listening_words.p115')"></div>
+
+        <figure>
+          <figcaption>{{ t('det_listening_words.figcaption1') }}</figcaption>
+        </figure>
+
+        <p>{{ t('det_listening_words.p116') }}</p>
+        <p>{{ t('det_listening_words.p117') }}</p>
+        <p>{{ t('det_listening_words.p118') }}</p>
+        <p>{{ t('det_listening_words.p119') }}</p>
+        <p>{{ t('det_listening_words.p120') }}</p>
+        <p>{{ t('det_listening_words.p121') }}</p>
+        <p>{{ t('det_listening_words.p122') }}</p>
+        <p>{{ t('det_listening_words.p123') }}</p>
+        <p>{{ t('det_listening_words.p124') }}</p>
+        <p>{{ t('det_listening_words.p125') }}</p>
+        <p>{{ t('det_listening_words.p126') }}</p>
+        <div class="html_wrapper" v-html="t('det_listening_words.p127')"></div>
+        <p>{{ t('det_listening_words.p128') }}</p>
+        <p>{{ t('det_listening_words.p129') }}</p>
+        <h2>{{ t('det_listening_words.h26') }}</h2>
+        <p>{{ t('det_listening_words.p130') }}</p>
+        <p>{{ t('det_listening_words.p131') }}</p>
+        <h2>{{ t('det_listening_words.h27') }}</h2>
+        <p>{{ t('det_listening_words.p132') }}</p>
+        <p>{{ t('det_listening_words.p133') }}</p>
+        <p>{{ t('det_listening_words.p134') }}</p>
+        <p>{{ t('det_listening_words.p135') }}</p>
+        <div class="html_wrapper" v-html="t('det_listening_words.ul1')"></div>
+        <!-- <div class="html_wrapper" v-html="t('det_listening_words.ul2')"></div> -->
+        <!-- <div class="html_wrapper" v-html="t('det_listening_words.p136')"></div> -->
+      </div>
+      <div class="common_prep_right"></div>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+@import '@/assets/prep.scss';
+</style>
+<style lang="scss">
+.det-listening-words {
+  .sub-item {
+    margin-left: 20px;
+  }
+  .orange {
+    color: orange;
+  }
+  .bg_yellow {
+    background-color: yellow;
+    width: fit-content;
+  }
+  .title {
+    text-align: center;
+    font-weight: bold;
+    font-size: 1.5em;
+  }
+  li {
+    .highlight {
+      background-color: yellow;
+    }
+  }
+  ul {
+    list-style-type: none;
+    padding-left: 16px;
+    li {
+    }
+    .header {
+      background-color: yellow;
+      font-weight: bold;
+
+      padding: 2px 5px;
+    }
+    .blue {
+      color: blue;
+    }
+    .indent {
+      margin-left: 20px;
+    }
+    .red {
+      color: red;
+    }
+    .sub-indent {
+      margin-left: 40px;
+    }
+    .purple {
+      color: purple;
+      text-decoration: underline;
+    }
+  }
+  p {
+    .blue {
+      color: blue;
+    }
+    .red {
+      color: red;
+    }
+  }
+  .html_wrapper {
+    .red-text {
+      color: #ff3300;
+    }
+    .blue-text {
+      color: #3366cc;
+    }
+    .orange-text {
+      color: orange;
+    }
+    .has-accent-3-color {
+      color: orange !important;
+    }
+  }
+  .wp-block-list {
+    margin-left: 0px;
+    padding-left: 20px;
+  }
+  .answer_out {
+    margin-top: 20px;
+    padding: 15px;
+    background-color: #f1f1f1;
+    border-radius: 4px;
+    text-align: left;
+    font-size: 16px;
+    line-height: 26px;
+
+    color: rgb(17, 17, 17);
+    background-color: rgb(241, 241, 241);
+  }
+  img {
+    max-width: 400px;
+    margin-top: 4px;
+  }
+}
+</style>
