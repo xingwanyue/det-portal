c1edd3935f97e815ff5f6a760b56c4fb96c2fa83
xingcongcong <xingcongcong@henanyuexue.com>
2025-03-11 09:47:05 +0800

    app.vue 内js逻辑注释

--- src/app.vue
+++ src/app.vue
@@ -1,28 +1,29 @@
 <script setup lang="ts">
-import { useRoute } from 'vue-router';
-import { useI18n } from 'vue-i18n';
+// import { useRoute } from 'vue-router';
+// import { useI18n } from 'vue-i18n';
 
-const route = useRoute();
-const { locale } = useI18n();
+// const route = useRoute();
+// const { locale } = useI18n();
 
-watch(
-  () => route.path,
-  (val) => {
-    if (val && process.client) {
-      try {
-        const currentPathWithoutLocale = route.path.replace(new RegExp(`^/${locale.value}`), '') || '/';
-        useFetch(
-          `https://www.duolingopractice.com/weapp/api/common/logPath?path=${encodeURIComponent(
-            currentPathWithoutLocale,
-          )}&locale=${locale.value}`,
-        );
-      } catch (e) {
-        //
-      }
-    }
-  },
-  { immediate: true },
-);
+// watch(
+//   () => route.path,
+//   (val) => {
+//     if (val && process.client) {
+//       try {
+//         const currentPathWithoutLocale = route.path.replace(new RegExp(`^/${locale.value}`), '') || '/';
+//         useFetch(
+//           `https://www.duolingopractice.com/weapp/api/common/logPath?path=${encodeURIComponent(
+//             currentPathWithoutLocale,
+//           )}&locale=${locale.value}`,
+//         );
+//       } catch (e) {
+//         //
+//       }
+//     }
+//   },
+//   { immediate: true },
+// );
+//
 </script>
 
 <template>
